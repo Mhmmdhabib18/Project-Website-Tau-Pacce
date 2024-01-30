8e8d13120af63c98b2849a1a73e998ce98cb4d51
Mhmmdhabib18 <mhmmdhabib987@gmail.com>
2024-01-30 09:56:02 -1200

    add responsive navbar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".nav-links");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
